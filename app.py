a2d99e9eaae92c8b96320ed739e9fdb7437504f8
Amna Bin Nashi <amnabinnashi2000@gmail.com>
2026-04-16 01:54:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from scraper import analyze_site
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def home():
-    data = None
+    return render_template('index.html')
 
-    if request.method == "POST":
-        url = request.form.get("url")
-        data = analyze_site(url)
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    url = data.get('url')
 
-    return render_template("index.html", data=data)
+    if not url:
+        return jsonify({"error": "اكتب رابط"}), 400
+
+    result = analyze_site(url)
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
